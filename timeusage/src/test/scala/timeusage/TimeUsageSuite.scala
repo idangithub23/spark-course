07e500780a32bf2acbc995f7d5638fc0b80b09a0
Michael <michael.slaw.delta@gmail.com>
2024-01-02 10:14:36 +0200

    Removed projects

--- timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
+++
@@ -1,8 +0,0 @@
-package timeusage
-
-import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import scala.util.Random
-import scala.util.Properties.isWin
-
-class TimeUsageSuite extends munit.FunSuite:
-  println("You can add tests here")
